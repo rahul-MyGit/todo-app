46e43801ba632d417300d5b7b650fd5119b661f2
Rahul <rahul.mymail1@gmail.com>
2025-11-13 13:34:04 +0530

    BE DONE

--- index.js
+++ index.js
@@ -16,8 +16,17 @@ app.get("/signup", (req, res) => {
 app.post("/signup", (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
-    const fileContents = fs.readFileSync("users.txt", "utf-8"); // 
+    
+    const fileContents = fs.readFileSync("users.txt", "utf-8");
     const fileContentsObject = JSON.parse(fileContents);
+
+    const userExists = fileContentsObject.users.some(user => user.username === username && user.password === password);
+    if (userExists) {
+        return res.send({
+            success: false,
+            message: "User already exists"
+        });
+    }
 
     fileContentsObject.users.push({
         id: userCounter,
@@ -30,24 +39,89 @@ app.post("/signup", (req, res) => {
     fs.writeFileSync("users.txt", JSON.stringify(fileContentsObject))
 
     res.send({
-        id: userCounter - 1
+        id: userCounter - 1,
+        success: true
     })
 })
 
-app.get("/dashboard", (Req, res) => {
+app.get("/dashboard", (req, res) => {
     res.sendFile(__dirname + "/todos.html")
 })
 
 app.post("/todo", (req, res) => {
-    res.send("hi there")
+    const userId = req.body.userId;
+    const todoText = req.body.todo;
+    
+    const fileContents = fs.readFileSync("users.txt", "utf-8");
+    const fileContentsObject = JSON.parse(fileContents);
+    
+    const user = fileContentsObject.users.find(u => u.id === userId);
+    
+    if (!user) {
+        return res.status(404).send({ success: false, message: "User not found" });
+    }
+    
+    const todoId = user.todos.length > 0 ? Math.max(...user.todos.map(t => t.id)) + 1 : 0;
+    
+    user.todos.push({
+        id: todoId,
+        text: todoText,
+        completed: false
+    });
+    
+    fs.writeFileSync("users.txt", JSON.stringify(fileContentsObject));
+    
+    res.send({
+        success: true,
+        todoId: todoId
+    });
 })
 
 app.delete("/todo", (req, res) => {
-    res.send("hi there")
+    const userId = req.body.userId;
+    const todoId = req.body.todoId;
+    
+    const fileContents = fs.readFileSync("users.txt", "utf-8");
+    const fileContentsObject = JSON.parse(fileContents);
+    
+    const user = fileContentsObject.users.find(u => u.id === userId);
+    
+    if (!user) {
+        return res.status(404).send({ success: false, message: "User not found" });
+    }
+    
+    const todoIndex = user.todos.findIndex(t => t.id === todoId);
+    
+    if (todoIndex === -1) {
+        return res.status(404).send({ success: false, message: "Todo not found" });
+    }
+    
+    user.todos.splice(todoIndex, 1);
+    
+    fs.writeFileSync("users.txt", JSON.stringify(fileContentsObject));
+    
+    res.send({
+        success: true,
+        message: "Todo deleted"
+    });
 })
 
 app.get("/todo", (req, res) => {
-
+    const userId = parseInt(req.query.userId);
+    
+    const fileContents = fs.readFileSync("users.txt", "utf-8");
+    const fileContentsObject = JSON.parse(fileContents);
+    
+    const user = fileContentsObject.users.find(u => u.id === userId);
+    
+    if (!user) {
+        return res.status(404).send({ success: false, message: "User not found" });
+    }
+    
+    res.send({
+        success: true,
+        todos: user.todos
+    });
 })
 
 app.listen(3000);
